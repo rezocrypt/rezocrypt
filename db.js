e8102968f931e0ce767f8d068c139e87ecb621d4
Davit Harutyunyan <87856097+rezocrypt@users.noreply.github.com>
2026-02-23 06:59:56 +0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,300 @@
+window.DB =
+{
+  "profile": {
+    "name": "Rezocrypt (Davit Harutyunyan)",
+    "headline": "Systems Engineer — Everything is a System",
+    "avatar": "./logo.png"
+  },
+  "about": [
+    "Hi, I am Rezocrypt. This is not a CV for work or a list of pointless achievements, and I included the ones that matter to me. This is a personal story about what I have done so far and what I am still working on. Now a little bit about me.",
+    "I have been inspired by software and hardware design from an early age. However, I never accepted it as a profession; instead, I chose to remain free. In 2025, I started exploring the following topics, where different dots in my unorganized knowledge connected to each other: Calculus, Linear Algebra, Discrete Math, Physics, Complexity Theory, Decentralization, Information Theory, Consciousness Theory, and Systems Thinking. I listed these topics because they caused a huge paradigm shift for me and give some idea about my interests.",
+    "Additionally, the idea of open source inspired me, and I decided to contribute because it’s amazing when you solve a problem for yourself and then publish it for others who may have that problem too. As a major advantage, trying to understand the architecture of large open-source software (like Linux and Node.js) shaped my software engineering skills.",
+    "Feel free to ask any questions or have any discussion (I would appreciate it). Now you can explore more about me below."
+  ],
+  "specializations": [
+    {
+      "title": "Network Engineering",
+      "details": "Since early 2023, focused on mastering Network Engineering. Passed the Network Engineering exam ATMK and gained two years of practical experience at the Ministry of Defense."
+    },
+    {
+      "title": "Software Design",
+      "details": "Since 2020, developed simple but interesting programs such as Conway’s Game of Life and terminal tools. After 2024, built a deeper understanding of software engineering, analyzed open-source projects like the Linux kernel and FFmpeg, contributed to open source, and started building personal projects to solve real problems and encourage contributions."
+    },
+    {
+      "title": "OSINT Engineer",
+      "details": "Have a lot of practice in open-source intelligence methods and structured information analysis."
+    },
+    {
+      "title": "Linux",
+      "details": "Working with Linux since 2020, including Kali, Mint, and Debian. Studied distribution differences and how complete systems are built on top of the kernel. Also explored FreeBSD."
+    },
+    {
+      "title": "Darknets",
+      "details": "Studied Tor and especially I2P, analyzing how these networks function as systems and examining their architectural vulnerabilities."
+    }
+  ],
+  "hobbies": [
+    {
+      "title": "Reading",
+      "details": "One of the oldest hobbies since childhood. Read 134 books and built a personal library of around 1,000 books."
+    },
+    {
+      "title": "Mathematics",
+      "details": "Since 2025, developed strong interest in abstract mathematics, including set theory, Gödel’s theorems, complexity theory, and field theory. Interested in multidimensional spaces and their applications in analyzing complex systems. Studied Calculus I and II, linear algebra, and discrete mathematics."
+    },
+    {
+      "title": "Cryptography",
+      "details": "Interested in combining cryptographic concepts with modular arithmetic and deeper mathematical structures."
+    },
+    {
+      "title": "Fast Typing",
+      "details": "Practicing touch typing since 2023, averaging around 110 WPM in a 2-minute Monkeytype test."
+    },
+    {
+      "title": "Anonymity",
+      "details": "Interested in anonymity systems, studying systematic methods and their vulnerabilities. Understand that anonymity is not absolute but a matter of increasing the cost of identification."
+    },
+    {
+      "title": "Podcasts",
+      "details": "Since 2025, actively listening to Armenian podcasters such as Rearrange and Rock Talk, and international podcasts including Lex Fridman Podcast, The Joe Rogan Experience, and Huberman Lab."
+    }
+  ],
+  "contact": {
+    "emails": [
+      "rezocrypt@gmail.com",
+      "davit_harutyunyan3@edu.aua.am"
+    ],
+    "telegram": "https://t.me/rezocrypt",
+    "github": "https://github.com/rezocrypt",
+    "note": "Contact me on Telegram for the fastest response."
+  },
+  "formalEducation": [
+    {
+      "school": "Yerevan basic school № 132 named after Admiral Isakov",
+      "dates": "2010–2017",
+    },
+    {
+      "school": "PhysMath School after A. Shahinyan",
+      "dates": "2017–2023",
+    },
+    {
+      "school": "American University of Armenia",
+      "dates": "Aug 2023 – Dec 2023 (Army break); Jan 2026 – Present",
+      "description": "Bachelor of Science in Engineering Science"
+    }
+  ],
+  "tools": [
+    "HTML5",
+    "Arduino",
+    "Assembly",
+    "CSS3",
+    "Javascript",
+    "NodeJS",
+    "C",
+    "Linux",
+    "Tor",
+    "I2P",
+    "Bitcoin",
+    "OSINT",
+    "RouterOS"
+  ],
+  "workExperience": [
+    {
+      "company": "PinCat Security System",
+      "role": "Junior Network Engineer",
+      "dates": "Sep 2023–Jan 2024"
+    },
+    {
+      "company": "Ministry of Defense",
+      "role": "Software & Network Engineer",
+      "dates": "Jan 2024–Jan 2026",
+      "description": "Developed and implemented internal technical solutions."
+    },
+    {
+      "company": "PinCat Security System",
+      "role": "Network Engineering Educator (Intermediate Level)",
+      "dates": "Feb 2024–Present",
+      "description": "Teach intermediate network engineering, explain core concepts in a structured way, and guide students through practical configurations and real-world scenarios."
+    }
+  ],
+  "projects": [
+    {
+      "name": "AUA-Utils",
+      "dates": "Jan 2026–Present",
+      "role": "Developer & Maintainer",
+      "description": "AUA Utils offers tools for American University of Armenia students to simplify and enhance workflow across AUA web platforms.",
+      "url": "https://chromewebstore.google.com/detail/echkbfkpejnpbjfmjoagfijakbnbglgp?utm_source=item-share-cb"
+    },
+    {
+      "name": "Takanqvardan.am",
+      "dates": "Jan 2024–Present",
+      "role": "Developer & Maintainer",
+      "description": "Armenian fan platform dedicated to the Kargin TV shows.",
+      "url": "https://takanqvardan.am"
+    }
+  ],
+  "achievements": [
+    {
+      "name": "IoT Hackathon – Smart Botanic",
+      "dates": "November 2023",
+      "description": "Worked on developing and presenting a smart IoT solution as part of a team. Won second place."
+    },
+    {
+      "name": "DigiCamp innovative triathlon camp (by UATE)",
+      "dates": "Jul 13-Jul 25 2022",
+      "description": "Built a sandwich-making robot (but didn't finish), scored 4th from 28, was the leader/engineer of the \"Foodbot\" team."
+    },
+    {
+      "name": "Hawk UAV Competition 2022",
+      "dates": "Sep 9–Sep 11, 2022",
+      "description": "Designed and built a firefighting quadcopter with a custom watering mechanism. Acted as team leader and pilot. Passed the first round; did not advance further."
+    }
+  ],
+  "unformalEducation": [
+    {
+      "name": "Smart City Educational Program",
+      "dates": "2014–2016",
+      "description": "Completed the “Robotics and Programming” course at Smart City. Learned fundamental programming blocks at an early age, which helped shape systems thinking. Participated in Digitec EXPO 2016.",
+      "image": "./certificates/CERTIFICATE_Smart_City.jpg"
+    },
+    {
+      "name": "SpaceCamp 2022 (by Ministry of High-Tech Industry, AYAS)",
+      "dates": "Jul 27–Aug 2, 2022",
+      "description": "Attended daily lectures, built and tested rocket models, and received open-source satellite data. Ranked top 2 out of 10 in the best cosmic idea presentation.",
+      "image": "./certificates/CERTIFICATE_SpaceCamp_2022.jpg"
+    },
+    {
+      "name": "Engineering City – Electronics Program",
+      "dates": "Mar 5–Jun 5, 2023",
+      "description": "Completed electronics training covering fundamental circuit concepts and practical applications. Certificate issued Nov 5, 2023.",
+      "image": "./certificates/CERTIFICATE_Engineering_City_Electronics.jpg"
+    },
+    {
+      "name": "Network and System Administration Advanced Course – PinCat",
+      "dates": "Jan 1, 2023–Jan 1, 2024",
+      "description": "Completed System and Network Administration (Pre-Intermediate) and Network Administration and Security (Advanced). Achieved Grade A+. Covered OS, virtualization, networking, routing and switching, servers, firewall configuration, VPN, and encryption.",
+      "image": "./certificates/CERTIFICATE_Pincat_System_and_Network_administration_advanced.jpg"
+    },
+    {
+      "name": "Unmanned Aerial Vehicle Course – AUA, Karen Vardanyan Educational Foundation",
+      "dates": "Mar 4–Apr 3, 2022",
+      "description": "One-month online course. Visited UAV factories. Built and flew an airplane/drone in FlyLab.",
+      "image": "./certificates/CERTIFICATE_UAV_course.jpg"
+    },
+    {
+      "name": "The Basics of Analog Electronics – ANEL",
+      "dates": "Oct 24–Nov 24, 2022",
+      "description": "Worked with electronic components, built circuits, generated and measured analog signals.",
+      "image": "./certificates/CERTIFICATE_the_basics_of_analog_electronics.jpg"
+    },
+    {
+      "name": "Embedded Systems – Instigate CJSC",
+      "dates": "Apr 4–Aug 24, 2022",
+      "description": "Learned PCB design, Linux OS basics, and compiling fundamentals. Explored electronics and physics. Completed an additional one-month apprenticeship.",
+      "image": "./certificates/CERTIFICATE_Instigate_robotix_course.jpg"
+    },
+    {
+      "name": "Armath in the Air – UAV Club (UATE)",
+      "dates": "Apr 9, 2022–Jan 1, 2024",
+      "description": "Learned to build fixed-wing and multirotor UAVs, fly them, and adjust them for practical needs."
+    },
+    {
+      "name": "Armath Laboratory",
+      "dates": "Apr 2019–Jan 2022",
+      "description": "Learned to work with Arduino and other hardware sensors and modules (such as ESP8266) to develop smart devices."
+    },
+    {
+      "name": "Tumo Center for Creative Technologies",
+      "dates": "Jan 12, 2017–Sep 2023",
+      "description": "Participated in STEM-related classes. Gained skills in robotics, coding, and web design.",
+      "image": "./certificates/CERTIFICATE_tumo.jpg"
+    },
+    {
+      "name": "Basic IT Center – Python Programming Full Course",
+      "dates": "Sep 8–Dec 8, 2020",
+      "description": "Learned Python fundamentals and OOP, Django framework. Built an online shop website as a final project.",
+      "image": "./certificates/CERTIFICATE_Basic_IT_Center-Python.jpg"
+    },
+    {
+      "name": "Cybersecurity Training",
+      "dates": "Jun 30–Jul 30, 2023",
+      "description": "Participated in a one-month (12-hour) training under the Armenian National Supercomputing Center Establishment program. Covered fundamental cybersecurity concepts and practical security measures.",
+      "image": "./certificates/CERTIFICATE_Cybersecurity_AITC.jpg"
+    },
+    {
+      "name": "Cybersecurity Bootcamp",
+      "dates": "Sep 2023",
+      "description": "Completed a 20-hour cybersecurity bootcamp organized by CyHub Armenia and Microsoft Innovation Center Armenia. Focused on practical cybersecurity skills and threat analysis fundamentals.",
+      "image": "./certificates/CERTIFICATE_Cyhub_cybersecurity_bootcamp.jpg"
+    }
+  ]
+  ,
+  "contributions": [
+    {
+      "name": "XaoS",
+      "what": "Fractal visualization software",
+      "did": "Implemented additional fractal example configurations, improved documentation, and added Armenian translation.",
+      "url": "https://github.com/xaos-project/XaoS",
+      "logo": "./contributings/xaos.png"
+    },
+    {
+      "name": "Htop",
+      "what": "Interactive CLI process viewer.",
+      "did": "Added missing Armenian localization entry.",
+      "url": "https://github.com/htop-dev/htop",
+      "logo": "./contributings/htop.png"
+    },
+    {
+      "name": "Sherlock",
+      "what": "Username OSINT tool.",
+      "did": "Added search capability over multiple useful platforms.",
+      "url": "https://github.com/sherlock-project/sherlock",
+      "logo": "./contributings/sherlock.png"
+    }
+  ],
+  "personal": {
+    "fullName": "Davit Harutyunyan",
+    "location": "Yerevan, Armenia",
+    "age": 20,
+    "languages": [
+      {
+        "name": "Armenian",
+        "level": "Native"
+      },
+      {
+        "name": "English",
+        "level": "B2–C1"
+      },
+      {
+        "name": "Russian",
+        "level": "Intermediate"
+      }
+    ]
+  },
+  "volunteerings": [
+    {
+      "name": "Armenian Paragliding Accuracy Cup",
+      "date": "August 19, 2023",
+      "description": "Served as a referee, measuring each pilot’s landing distance from the center target to determine scoring accuracy.",
+      "image": "./volunteerings/VOLUNTEERING_CERTIFICATE_Paragliding_Accuracy_Cup.JPG"
+    },
+    {
+      "name": "Public Accountability and Journalism Conference",
+      "date": "May 4, 2023",
+      "description": "Assisted in organizing and coordinating the media conference, supporting logistics and on-site operations.",
+      "image": "./volunteerings/VOLUNTEERING_CERTIFICATE_Media_Conference.JPG"
+    },
+    {
+      "name": "Russian Film Festival in Yerevan",
+      "date": "October 21–23, 2022",
+      "description": "Supported event organization and operational tasks during the festival.",
+      "image": "./volunteerings/VOLUNTEERING_CERTIFICATE_DeeM_communications_RFF.jpg"
+    },
+    {
+      "name": "The Luxe Expo 2023",
+      "date": "April 28–30, 2023",
+      "description": "Contributed to event coordination and assisted participants during the exhibition.",
+      "image": "./volunteerings/VOLUNTEERING_CERTIFICATE_Lux_Expo.jpg"
+    }
+  ]
+}
